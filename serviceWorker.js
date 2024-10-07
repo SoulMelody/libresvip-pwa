e7741aeb37eb3e63ff7b2c4077e374acfbdaa2b6
SoulMelody <SoulMelody@users.noreply.github.com>
2024-10-07 06:17:04 +0000

    deploy: ac0d26de1b5ace019a9f5919cf058bc7d9681ae1

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,5 @@
 const appName = 'Panel Pyodide App'
-const appCacheName = 'Panel Pyodide App-4ac6d3bf699d4fc9aebf8fc5486799e1';
+const appCacheName = 'Panel Pyodide App-ebbb41e26cde4bc5b782e7ce6f996497';
 
 const preCacheFiles = ['images/favicon.ico', 'images/icon-vector.svg', 'images/icon-32x32.png', 'images/icon-192x192.png', 'images/icon-512x512.png', 'images/apple-touch-icon.png', 'images/index_background.png'];
 
