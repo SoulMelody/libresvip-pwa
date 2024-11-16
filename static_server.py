86ed19396d045dbb754b8a24674a18c1d3100bc0
SoulMelody <SoulMelody@users.noreply.github.com>
2024-11-16 11:25:31 +0800

    update workflow file

---
+++ static_server.py
@@ -0,0 +1,32 @@
+import pathlib
+
+import tornado.web
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self) -> None:
+        self.add_header("Content-Type", "text/html")
+        self.write(pathlib.Path("dist/index.html").read_bytes())
+
+
+class MyStaticFileHandler(tornado.web.StaticFileHandler):
+    def get_content_type(self) -> str:
+        path_suffix = pathlib.Path(self.absolute_path).suffix
+        if path_suffix == ".js":
+            return "application/javascript"
+        return super().get_content_type()
+
+
+current_path = pathlib.Path(__file__).parent / "dist"
+app = tornado.web.Application(
+    [
+        (r'^/$', IndexHandler),
+        (r'^/(.+)$', MyStaticFileHandler, {"path": current_path}),
+    ],
+    static_path=current_path,
+)
+
+if __name__ == '__main__':
+    app.listen(8000, '127.0.0.1')
+    print("Starting server on http://127.0.0.1:8000")
+    tornado.ioloop.IOLoop.current().start()
