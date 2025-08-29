08ee726817ed133f63c2640584b6be8b293d5023
SoulMelody <SoulMelody@users.noreply.github.com>
2025-08-29 01:43:19 +0000

    deploy: 85943957e266b78945cbc5921fc0d69e94105bfe

--- assets/Hooks-BJjV32tK-BahC7Lq-.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-cnoSlX5w.js";const c=r=>{const t=e.useRef();return e.useEffect(()=>{t.current=r},[r]),t.current};export{c as o};
---
+++ assets/InputInstructions-Dg4vuFj3-DD0t28ey.js
@@ -0,0 +1,5 @@
+import{cX as d,j as c,cY as g,c as h,k as p}from"./index-CWdNveUj.js";const $=p`
+  50% {
+    color: rgba(0, 0, 0, 0);
+  }
+`,f=h("span",{target:"edlqvik0"})(({includeDot:e,shouldBlink:n,theme:t})=>({...e?{"&::before":{opacity:1,content:'"•"',animation:"none",color:t.colors.gray,margin:`0 ${t.spacing.twoXS}`}}:{},...n?{color:t.colors.red,animationName:`${$}`,animationDuration:"0.5s",animationIterationCount:5}:{}}),""),x=({dirty:e,value:n,inForm:t,maxLength:s,className:m,type:r="single",allowEnterToSubmit:u=!0})=>{const o=[],i=(a,l=!1)=>{o.push(c.jsx(f,{includeDot:o.length>0,shouldBlink:l,children:a},o.length))};if(u){const a=t?"submit form":"apply";if(r==="multiline"){const l=d()?"⌘":"Ctrl";i(`Press ${l}+Enter to ${a}`)}else r==="single"&&i(`Press Enter to ${a}`)}return s&&(r!=="chat"||e)&&i(`${n.length}/${s}`,e&&n.length>=s),c.jsx(g,{"data-testid":"InputInstructions",className:m,children:o})};export{x as b};
