e7f4322ce942ce4cd523a6b43b71e02437c7d852
SoulMelody <SoulMelody@users.noreply.github.com>
2025-10-19 22:25:45 +0800

    update libresvip to 2.0.1

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { mount } from "@stlite/browser";
 import stliteLibWheel from "@stlite/browser/wheels/stlite_lib-0.1.0-py3-none-any.whl";
 import streamlitWheel from "@stlite/browser/wheels/streamlit-1.48.0-cp313-none-any.whl";
-import libresvipWheel from "./assets/libresvip-2.0.0-py3-none-any.whl";
+import libresvipWheel from "./assets/libresvip-2.0.1-py3-none-any.whl";
 import constructWheel from "./assets/construct-2.10.68-py3-none-any.whl";
 import pycryptodomexWheel from "./assets/pycryptodomex-3.21.0-cp36-abi3-pyodide_2025_0_wasm32.whl";
 import wanakanaWheel from "./assets/wanakana_python-1.2.2-py3-none-any.whl";
